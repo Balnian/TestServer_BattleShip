f765b0b6cea7c06cccf02492aa0e783c0209e9e4
Francis <super_f_qc@hotmail.com>
2015-10-13 21:14:19 -0400

    modif

--- TestServer/Program.cs
+++ TestServer/Program.cs
@@ -3,6 +3,7 @@ using System.Collections.Generic;
 using System.Linq;
 using System.Net.Sockets;
 using System.Text;
+using System.Threading;
 using System.Threading.Tasks;
 
 namespace TestServer
@@ -13,9 +14,13 @@ namespace TestServer
         {
             try
             {
-                TcpClient sdf = new TcpClient("p104-14", 1521);
-                sdf.Connect("127.0.0.1", 4242);
+                TcpClient sdf = new TcpClient("p104-14", 8080);
+                //sdf.Connect("127.0.0.1", 8080);
                 Console.WriteLine("Connecté?");
+                while (true)
+                {
+                    Thread.Sleep(250);
+                };
                 
             }
             catch (Exception e)
